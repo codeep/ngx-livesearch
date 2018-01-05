e18cd7d96a3df567083187dcf75965f69abd79ad
Stepan <styopdev@hotmail.com>
2018-01-05 19:44:09 +0400

    Structure changes

---
+++ src/app/ngx-livesearch/livesearch/livesearch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'livesearch-livesearch',
+  templateUrl: './livesearch.component.html',
+  styleUrls: ['./livesearch.component.css']
+})
+export class LivesearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

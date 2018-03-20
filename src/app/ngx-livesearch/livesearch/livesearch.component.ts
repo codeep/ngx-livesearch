39735c86d5bab69ff084fa81f67e1ddb617820c0
Stepan Vardanyan <styopdev@hotmail.com>
2018-03-20 14:33:32 +0400

    Changed results animation

--- src/app/ngx-livesearch/livesearch/livesearch.component.ts
+++ src/app/ngx-livesearch/livesearch/livesearch.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, HostListener, Output, Renderer2,
-        EventEmitter, OnDestroy, ElementRef, ContentChild, 
-        TemplateRef, Optional, ViewChild, AfterViewInit } from '@angular/core';
+         EventEmitter, OnDestroy, ElementRef, ContentChild, 
+         TemplateRef, Optional, ViewChild, AfterViewInit 
+       } from '@angular/core';
+
 import { Observable } from 'rxjs/Rx';
 import {Subject} from 'rxjs/Subject';
 import { Subscription } from 'rxjs/Subscription';
@@ -18,16 +20,15 @@ import { SearchResultHighlightDirective } from '../directives/search-result-high
   styleUrls: ['./livesearch.component.css'],
   animations: [
     trigger('listAnimation', [
-      transition('* => *', [
-        query(':enter', style({ opacity: 0 }), {optional: true}),
-        query(':enter', stagger('30ms', [
-          animate('.2s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
-          ]))]), {optional: true})
+      transition('* => void', [
+        style({opacity: 1, overflow: 'hidden' }),
+        animate(350, style({ height: 0, opacity: 0 }))
+      ]),
+      transition('void => *', [
+        style({opacity: 0 }),
+        animate(500, style({ height: '*', opacity: 1 }))
       ])
     ])
-
   ]
 })
 export class LivesearchComponent implements OnInit, OnDestroy, AfterViewInit {
